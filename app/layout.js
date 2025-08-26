09ef42f7f2370e640cb1032ac6ad717eb893ca4f
Rajveer Singh <777rajveersingh@gmail.com>
2025-08-26 14:06:24 +0530

    write your own font-famitly called 'inter'

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 export const metadata = {
   title: "Pixxel", // change this to your app's name
   description: "AI Image Editor", // change this to your app's description
@@ -21,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
          <ThemeProvider
             attribute="class"
